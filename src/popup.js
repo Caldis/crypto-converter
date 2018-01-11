a98ed55dbe0e245b89ccc50349fa998c0e8d3a1e
Cyrus <532607446@qq.com>
2018-01-11 23:45:24 +0800

    更新名字 | Update name

--- src/popup.js
+++
@@ -1,7 +0,0 @@
-// React Libs
-import React from 'react'
-import ReactDOM from 'react-dom'
-// Components
-import Panel from "@/components/Panel"
-
-ReactDOM.render(<Panel popup={true}/>, document.getElementById('root'))
